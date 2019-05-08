e5428f3ab0a84df9b42eb011c3732a11e8b82459
Yang1013 <34757634+Yang1013@users.noreply.github.com>
2019-05-08 11:09:13 +0800

    Delete ECCDefine.v

--- src/ECCDefine.v
+++
@@ -1,9 +0,0 @@
-
-parameter MAX_BITS = 256;
-parameter MAX_REG = 7;  // lg(MAX_BITS) - 1
-
-// operating mode
-parameter BITS256 = 2'b11;
-parameter BITS128 = 2'b10;
-parameter BITS64  = 2'b01;
-parameter BITS32  = 2'b00;
